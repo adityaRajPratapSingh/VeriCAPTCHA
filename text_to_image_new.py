aefb49cfa795a1ee82fc580f4697f93d7cc20804
adityaRajPratapSingh <magellan2409@gmail.com>
2024-08-21 16:46:50 +0530

    made the sentence even more smaller

--- text_to_image_new.py
+++ text_to_image_new.py
@@ -5,15 +5,15 @@ from PIL import Image
 
 def the_image(text:str):
     captcha:ImageCaptcha = ImageCaptcha(
-      width=700,
-      height = 400,
+      width=800,
+      height = 500,
       fonts = [
           './Jersey10-Regular.ttf',
           './Lato-Black.ttf',
           './OpenSans-Regular.ttf',
           './Roboto-Thin.ttf'
       ],
-      font_sizes=(70, 40, 100, 30)
+      font_sizes=(60, 40, 100)
     )
     data:BytesIO = captcha.generate(text)
     
