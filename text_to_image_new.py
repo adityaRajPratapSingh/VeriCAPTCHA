d15ade66e5cc699323f953b727db48434ebde061
adityaRajPratapSingh <magellan2409@gmail.com>
2024-08-21 17:59:23 +0530

    final changes

--- text_to_image_new.py
+++ text_to_image_new.py
@@ -6,14 +6,14 @@ from PIL import Image
 def the_image(text:str):
     captcha:ImageCaptcha = ImageCaptcha(
       width=800,
-      height = 300,
+      height = 100,
       fonts = [
           './Jersey10-Regular.ttf',
           './Lato-Black.ttf',
           './OpenSans-Regular.ttf',
           './Roboto-Thin.ttf'
       ],
-      font_sizes=(60, 40, 50)
+      font_sizes=(40, 50)
     )
     data:BytesIO = captcha.generate(text)
     
