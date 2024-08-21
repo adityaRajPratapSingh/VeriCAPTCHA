c9c5931a5fc73aab62405f17c4dea44841cdc1eb
adityaRajPratapSingh <magellan2409@gmail.com>
2024-08-21 17:19:24 +0530

    changes paramaters of text to image

--- database.py
+++ database.py
@@ -35,7 +35,7 @@ def return_a_random_document(db:str, collection:str):
     random_document_cursor = coll.find().skip(random_index).limit(1)
     random_document=list(random_document_cursor)[0]
     serealized = serialise_2(random_document)
-    if len(serealized['sentence'])>50:
+    if len(serealized['sentence'])>35:
         return return_a_random_document(db, collection)
     return random_document
 
--- text_to_image_new.py
+++ text_to_image_new.py
@@ -6,14 +6,14 @@ from PIL import Image
 def the_image(text:str):
     captcha:ImageCaptcha = ImageCaptcha(
       width=800,
-      height = 500,
+      height = 300,
       fonts = [
           './Jersey10-Regular.ttf',
           './Lato-Black.ttf',
           './OpenSans-Regular.ttf',
           './Roboto-Thin.ttf'
       ],
-      font_sizes=(60, 40, 100)
+      font_sizes=(60, 40, 50)
     )
     data:BytesIO = captcha.generate(text)
     
